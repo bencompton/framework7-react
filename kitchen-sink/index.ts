20b07b5d0b2703f681a9fbab02b9d3fade4a7a18
bc022699 <Ben.Compton@cerner.com>
2016-12-14 08:45:41 -0500

    Update kitchen sink

--- kitchen-sink/index.ts
+++
@@ -1,6 +0,0 @@
-import {render} from 'react-dom';
-import * as React from 'react';
-
-import {Routes} from './components/App';
-
-render(React.createElement(Routes), document.getElementById('react-root'));
