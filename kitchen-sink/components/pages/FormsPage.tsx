1df70f162f22f5fc3ea78b6bccad90fddda25ad1
jc022736 <jason.colley@cerner.com>
2017-01-11 15:16:06 -0500

    Finished forms for kitchen sink

--- kitchen-sink/components/pages/FormsPage.tsx
+++ kitchen-sink/components/pages/FormsPage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Page, Navbar, ContentBlockTitle, List, ListItem, ListLabel, FormLabel, FormInput} from 'framework7-react';
+import {Page, Navbar, Button, ButtonsSegmented, ContentBlock, ContentBlockTitle, GridCol, GridRow, List, ListItem, ListLabel, FormLabel, FormInput} from 'framework7-react';
 
 const onChangeHandler = (event) => {
     console.log('change');
@@ -34,7 +34,7 @@ export const FormsPage = () => {
                 </ListItem>                
                 <ListItem>
                     <FormLabel>Birth date</FormLabel>  
-                    <FormInput type="date" placeholder="Birth date" value="2014-04-30" />   
+                    <FormInput type="date" placeholder="Birth date" />   
                 </ListItem>
                 <ListItem>
                     <FormLabel>Date time</FormLabel>  
@@ -62,7 +62,7 @@ export const FormsPage = () => {
             </List>
 
             <ContentBlockTitle>Form With Floating Labels</ContentBlockTitle>
-            <List>
+            <List form>
                 <ListItem>
                     <FormLabel floating>Name</FormLabel>  
                     <FormInput type="text" placeholder="Name" />   
@@ -76,6 +76,88 @@ export const FormsPage = () => {
                     <FormInput type="email" placeholder="E-mail" />
                 </ListItem>
             </List>
+
+            <ContentBlockTitle>Form Without Labels</ContentBlockTitle>
+            <List form>
+                <ListItem> 
+                    <FormInput type="text" placeholder="Name" />   
+                </ListItem>
+                <ListItem>
+                    <FormInput type="password" placeholder="Password" />
+                </ListItem>
+                <ListItem> 
+                    <FormInput type="email" placeholder="E-mail" />
+                </ListItem>
+            </List>
+
+            <ContentBlockTitle>Checkboxes</ContentBlockTitle>
+            <List form>
+                {
+                    ['1', '2', '3'].map(n => {
+                        return (
+                            <ListItem
+                                checkbox
+                                key={n}
+                                name="my-checkbox"
+                                value={n}
+                                title={`Checkbox ${n}`}
+                            />
+                        );
+                    })
+                }
+            </List>
+
+            <ContentBlockTitle>Radios</ContentBlockTitle>
+            <List form>
+                {
+                    ['1', '2', '3'].map(n => {
+                        return (
+                            <ListItem
+                                radio
+                                key={n}
+                                name="my-radio"
+                                //checked={n === "1"}
+                                value={n}
+                                title={`Radio ${n}`}
+                            />
+                        );
+                    })
+                }
+            </List>
+
+            <ContentBlockTitle>Buttons</ContentBlockTitle>
+            <ContentBlock inner>
+                <p><Button>Button</Button></p>
+                <p><Button round>Button Round</Button></p>
+                <p><Button fill>Button Fill</Button></p>
+                <p>
+                    <ButtonsSegmented>
+                        <Button round active>Button 1</Button>
+                        <Button round>Button 2</Button>
+                        <Button round>Button 3</Button>
+                    </ButtonsSegmented>
+                </p>
+                <p>
+                    <ButtonsSegmented color="orange">
+                        <Button round big>Button 1</Button>
+                        <Button round big active>Button 2</Button>
+                        <Button round big>Button 3</Button>
+                    </ButtonsSegmented>
+                </p>
+                <p>
+                    <GridRow>
+                        <GridCol><Button big fill color="green">Send</Button></GridCol>
+                        <GridCol><Button big fill color="red">Delete</Button></GridCol>
+                    </GridRow>
+                </p>
+                <p>
+                    <GridRow>
+                        <GridCol><Button fill raised color="green">Raised</Button></GridCol>
+                        <GridCol><Button raised color="red">Raised</Button></GridCol>
+                        <GridCol><Button fill raised color="pink">Raised</Button></GridCol>
+                    </GridRow>
+                </p>
+            </ContentBlock>
         </Page>
     );
 }
