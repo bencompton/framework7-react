a528ac3a4691593dd713df99c500d7f4104f9bc6
bc022699 <Ben.Compton@cerner.com>
2017-01-25 11:56:35 -0500

    Optimize PropProcessor performance

--- src/utils/reactify-vue/react-element-creation/PropsProcessor.ts
+++ src/utils/reactify-vue/react-element-creation/PropsProcessor.ts
@@ -1,8 +1,89 @@
-import {camelCase} from 'change-case';
-import * as classNames from 'classnames';
 import * as React from 'react';
 
 import {IVueComponent} from '../ReactifyVue';
+
+const camelCase = (str) => {    
+    (camelCase as any).replacement = (camelCase as any).replacement || {};
+
+    if (!(camelCase as any).replacement[str]) {
+        (camelCase as any).replacement[str] = str.split('-').join('');
+    }
+
+    return (camelCase as any).replacement[str];
+};
+
+const hasOwn = {}.hasOwnProperty;
+
+const classNames = (...args: any[]) => {
+    const classes = [];    
+    let length = args.length;
+
+    while (length--) {
+        let  arg = args.pop();
+
+        if (!arg) continue;
+
+        let argType = typeof arg;
+
+        if (argType === 'string' || argType === 'number') {
+            classes.push(arg);
+        } else if (Array.isArray(arg)) {
+            classes.push(classNames.apply(null, arg));
+        } else if (argType === 'object') {
+            const keys = Object.keys(arg);
+            let length = keys.length;
+
+            while (length--) {
+                let key = keys.pop();
+
+                if (hasOwn.call(arg, key) && arg[key]) {
+                    classes.push(key);
+                }
+            }
+        }
+    }
+
+    return classes.join(' ');
+};
+
+const attributeMap = {
+    autocapitalize: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'autoCapitalize'
+    },
+    autocomplete: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'autoComplete'
+    },
+    autocorrect: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'autoCorrect'
+    },
+    autofocus: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'autoFocus'
+    },
+    autosave: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'autoSave'
+    },
+    maxlength: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'maxLength'
+    },
+    minlength: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'minLength'
+    },
+    readonly: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'readOnly'            
+    },
+    spellcheck: {
+        componentNames: ['input', 'textarea', 'select'],
+        renameTo: 'spellCheck'            
+    }     
+};
 
 const handleRefs = (element: HTMLElement, vueComponent: IVueComponent, events: {[eventName: string]: Function}) => {
     if (events) {
@@ -21,45 +102,6 @@ const handleRefs = (element: HTMLElement, vueComponent: IVueComponent, events: {
 };
 
 const renameAttribute = (componentName, attribute) => {
-    const attributeMap = {
-        autocapitalize: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'autoCapitalize'
-        },
-        autocomplete: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'autoComplete'
-        },
-        autocorrect: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'autoCorrect'
-        },
-        autofocus: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'autoFocus'
-        },
-        autosave: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'autoSave'
-        },
-        maxlength: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'maxLength'
-        },
-        minlength: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'minLength'
-        },
-        readonly: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'readOnly'            
-        },
-        spellcheck: {
-            componentNames: ['input', 'textarea', 'select'],
-            renameTo: 'spellCheck'            
-        }     
-    }
-
     const attributeMapForAttribute = attributeMap[attribute];
 
     if (attributeMapForAttribute && attributeMapForAttribute.componentNames.indexOf(componentName) > -1) {
@@ -70,23 +112,27 @@ const renameAttribute = (componentName, attribute) => {
 };
 
 export class PropsProcessor {
+    private cachedPropKebabCase: {[camelCasedProp: string]: string};
+
     public getProps(args, children, componentOrComponentName, resolvedComponent, vueComponent) {
         this.addCurrentComponentAsParentOfChildren(children, vueComponent);
+        
+        const props = {};
 
-        return {
-            ...this.getClasses(args, vueComponent),
-            ...this.getStyle(args, vueComponent),
-            ...this.getAdditionalClassName(vueComponent),
-            ...this.getAdditionalStyles(vueComponent),
-            ...this.getRef(args, vueComponent),
-            ...this.getPropsFromArgs(args),
-            ...this.getChildren(children, args),
-            ...this.convertAttrsToProps(args, componentOrComponentName, resolvedComponent),            
-            ...this.getInnerHTML(args)
-        };
+        this.getClasses(args, vueComponent, props);
+        this.getStyle(args, vueComponent, props);
+        this.getAdditionalClassName(vueComponent, props);
+        this.getAdditionalStyles(vueComponent, props);
+        this.getRef(args, vueComponent, props);
+        this.getPropsFromArgs(args, props);
+        this.getChildren(children, args, props);
+        this.convertAttrsToProps(args, componentOrComponentName, resolvedComponent, props);
+        this.getInnerHTML(args, props);        
+
+        return props;
     }
 
-    private getClasses(args, vueComponent) {
+    private getClasses(args, vueComponent, props) {
         let classObject = {};
 
         if (args.class && typeof args.class === 'string') {
@@ -100,13 +146,11 @@ export class PropsProcessor {
         }
 
         if (Object.keys(classObject).length) {
-            return { className: classNames(classObject) };
-        } else {
-            return {};
+            props.className = classNames(classObject);
         }        
     }
 
-    private getStyle(args, vueComponent) {
+    private getStyle(args, vueComponent, props) {
         let style;        
 
         if (args.style) {
@@ -118,112 +162,111 @@ export class PropsProcessor {
         }
 
         if (style) {
-            return { style };
-        } else {
-            return {};
+            props.style = style;
         }
     }
 
-    private getAdditionalClassName(vueComponent) {
+    private getAdditionalClassName(vueComponent, props) {
         //ClassName can be passed into any framework7-react component.
         //If it is passed in, add it as a prop.
         if (vueComponent.className) {
-            return { additionalClassName: vueComponent.className }
-        } else {
-            return {};
+            props.additionalClassName = vueComponent.className;
         }
     }
 
-    private getAdditionalStyles(vueComponent, ) {
+    private getAdditionalStyles(vueComponent, props) {
         //Style can be passed into any framework7-react component. 
         //If it is passed in, add it to any styles specified in the Vue component.
         if ((vueComponent as any).style) {
-            return {additionalStyles: (vueComponent as any).style };
-        } else { 
-            return {};
+            props.additionalStyles = (vueComponent as any).style;
         }
     }
 
-    private getRef(args, vueComponent) {
+    private getRef(args, vueComponent, props) {
         const refFunc = ((events, vueComponent) => {
             return (element: HTMLElement) => {
                 const events = args.on;
                 handleRefs(element, vueComponent, events);                
             };
         })(args.on, vueComponent);
-
-        return {
-            ref: refFunc,
-            refTemp: refFunc
-        };
-    }
-
-    private getPropsFromArgs(args) {
-        var props = {};
         
-        if (args.props) Object.keys(args.props).forEach(prop => props[camelCase(prop)] = args.props[prop]);
-
-        return props;
+        props.ref = refFunc;
+        props.refTemp = refFunc;        
     }
 
-    private getChildren(children, args) {
+    private getPropsFromArgs(args, props) {
+        if (args.props) {
+            const keys = Object.keys(args.props);
+            let length = keys.length;
+            
+            while (length--) {
+                let prop = keys.pop();
+                props[camelCase(prop)] = args.props[prop];
+            }
+        }        
+    }
+
+    private getChildren(children, args, props) {
         if (children && !(args.domProps && args.domProps.innerHTML)) {
-            return { children };
-        } else {
-            return {};
+            props.children = children;
         }
     }
 
-    private convertAttrsToProps(args, componentOrComponentName, resolvedComponent) {
-        const props = {};
+    private convertAttrsToProps(args, componentOrComponentName, resolvedComponent, props) {
+        if (args.attrs) {
+            const keys = Object.keys(args.attrs);
+            let length = keys.length;
 
-        if (args.attrs) Object.keys(args.attrs).forEach(attr => {
-            attr = renameAttribute(componentOrComponentName, attr);
+            while (length--) {
+                let attr = keys.pop();
 
-            const resolvedVueComponent = resolvedComponent.vueComponent;
-            const attrValue = args.attrs[attr];
-            
-            if (attrValue !== undefined || attrValue !== false) {
-                const camelCasedAttrName = camelCase(attr);
+                attr = renameAttribute(componentOrComponentName, attr);
 
-                if (resolvedVueComponent && resolvedVueComponent.props && resolvedVueComponent.props[camelCasedAttrName]) {
-                    if (resolvedVueComponent.props[camelCasedAttrName] === Boolean && attrValue  !== false) {
-                        props[camelCasedAttrName] = true;
+                const resolvedVueComponent = resolvedComponent.vueComponent;
+                const attrValue = args.attrs[attr];
+
+                if (attrValue !== undefined || attrValue !== false) {
+                    const camelCasedAttrName = camelCase(attr);
+                    const vueComponentProp = resolvedVueComponent && resolvedVueComponent.props && resolvedVueComponent.props[camelCasedAttrName];
+
+                    if (vueComponentProp) {
+                        if (vueComponentProp === Boolean && attrValue  !== false) {
+                            props[camelCasedAttrName] = true;
+                        } else {
+                            props[camelCasedAttrName] = attrValue;
+                        }
                     } else {
-                        props[camelCasedAttrName] = attrValue;
+                        props[attr] = attrValue;
                     }
-                } else {
-                    props[attr] = attrValue;
-                }
+                }                
             }
-        });
-
-        return props;
+        }        
     }
 
-    private getInnerHTML(args) {
+    private getInnerHTML(args, props) {
         let dangerouslySetInnerHTML;
 
         if (args.domProps && args.domProps.innerHTML) dangerouslySetInnerHTML = {__html: args.domProps.innerHTML};
 
         if (dangerouslySetInnerHTML) {
-            return { dangerouslySetInnerHTML };
-        } else {
-            return {};
+            props.dangerouslySetInnerHTML = dangerouslySetInnerHTML;
         }
     }
 
     private addCurrentComponentAsParentOfChildren(children, vueComponent) {
         if (children && Array.isArray(children) && children.length) {
-            children.forEach(child => {
-                if (child && child.props && child.type && typeof child.type !== 'string') {
-                    try {
+            for (let i = 0; i < children.length; i++) {
+                let child = children[i];
+
+                if (!child || !child.props) continue;                
+                if (!Object.getOwnPropertyDescriptor(child, 'props').writable) continue;
+
+                if (child.type && typeof child.type !== 'string') {                    
+                    if (!child.props.hasOwnProperty('ref')) {
                         child.props = {...child.props, parentVueComponent: vueComponent};
-                    } catch (err) {
-                        //console.warn(err);
                     }
                 }
-            });
+            }
         }
     }
 }
