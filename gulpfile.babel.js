cbaaaf888055c8edcaa713285f70b5d0b8f0f98c
bc022699 <Ben.Compton@cerner.com>
2017-03-10 14:05:20 -0500

    Ensure Framework7.js is capitalized to prevent issues on case-sensitive file systems. #19

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,7 +17,7 @@ gulp.task('clean', () => {
 
 gulp.task('build-framework7-core', ['clean'], () => {
     return merge(
-        gulp.src('./node_modules/framework7/dist/js/framework7.js')            
+        gulp.src('./node_modules/framework7/dist/js/Framework7.js')
             .pipe(replace('window.Framework7 = ', 'window.Framework7 = module.exports.Framework7 = '))
             .pipe(replace('window.Dom7 = ', 'window.Dom7 = module.exports.Dom7 = '))                        
             .pipe(replace('window.Template7 = ', 'window.Template7 = module.exports.Template7 = '))
