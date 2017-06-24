bdba53364f1b0960ce1ecfedc57fd55303b3b13c
Ben Compton <bencompton@users.noreply.github.com>
2017-06-24 10:33:24 -0400

    v0.9.1

--- dist/umd/js/framework7-react.js
+++ dist/umd/js/framework7-react.js
@@ -9,7 +9,7 @@
  * 
  * Licensed under APACHE 2.0
  * 
- * Released on: June 3, 2017
+ * Released on: June 24, 2017
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -9318,16 +9318,72 @@ exports.Views = Views_1.Views;
 
 "use strict";
 
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(4);
 var SelectableInput_1 = __webpack_require__(102);
 exports.ManagedFormInput = function (props) {
+    var renamedProps = {
+        name: props.name,
+        type: props.type,
+        placeholder: props.placeholder,
+        id: props.id,
+        value: props.valueComputed,
+        size: props.size,
+        accept: props.accept,
+        autoComplete: props.autocomplete,
+        autoCorrect: props.autocorrect,
+        autoCapitalize: props.autocapitalize,
+        spellCheck: props.spellcheck,
+        autoFocus: props.autofocus,
+        autoSave: props.autosave,
+        checked: props.checkedComputed,
+        disabled: props.disabled,
+        max: props.max,
+        maxLength: props.maxlength,
+        min: props.min,
+        minLength: props.minlength,
+        step: props.step,
+        multiple: props.multiple,
+        readOnly: props.readonly,
+        required: props.required,
+        color: props.color,
+        pattern: props.pattern,
+        onFocus: props.onFocus,
+        onBlur: props.onBlur,
+        onInput: props.onInput,
+        onChange: props.onChange,
+        onClick: props.onClick,
+        onKeyPress: props.onKeypress,
+        onKeyUp: props.onKeyUp,
+        onKeyDown: props.onKeyDown,
+        onCompositionStart: props.onCompositionstart,
+        onCompositionUpdate: props.onCompositionupdate,
+        onCompositionEnd: props.onCompositionend,
+        onDoubleClick: props.onDblclick,
+        onMouseDown: props.onMousedown,
+        onMouseEnter: props.onMouseenter,
+        onMouseLeave: props.onMouseleave,
+        onMouseMove: props.onMousemove,
+        onMouseOut: props.onMouseout,
+        onMouseOver: props.onMouseover,
+        onMouseUp: props.onMouseup,
+        onWheel: props.onWheel,
+        onSelect: props.onSelect
+    };
     switch (props.type.toLowerCase()) {
         case 'checkbox':
         case 'radio':
-            return React.createElement(SelectableInput_1.SelectableInput, { type: props.type, checked: props.checked, onChange: props.onChange });
+            return React.createElement(SelectableInput_1.SelectableInput, __assign({}, renamedProps));
         default:
-            return React.createElement("input", { type: props.type, onClick: props.onClick, onChange: props.onChange, value: props.value, placeholder: props.placeholder });
+            return React.createElement("input", __assign({}, renamedProps));
     }
 };
 
@@ -9348,6 +9404,14 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(4);
 var SelectableInput = (function (_super) {
@@ -9371,7 +9435,7 @@ var SelectableInput = (function (_super) {
         });
     };
     SelectableInput.prototype.render = function () {
-        return React.createElement("input", { type: this.props.type, ref: this.saveRef.bind(this) });
+        return React.createElement("input", __assign({ type: this.props.type, ref: this.saveRef.bind(this) }, this.props));
     };
     return SelectableInput;
 }(React.Component));
